02196109f713a6397f17317b34b7fb6205a22a05
Jesús Espino <jespinog@gmail.com>
2016-04-05 15:03:11 +0200

    Adding flags to the processor

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,6 +20,15 @@ impl IndexMut<Reg> for [u8] {
     }
 }
 
+struct Z80Flags {
+    s: bool,
+    z: bool,
+    h: bool,
+    p: bool,
+    n: bool,
+    c: bool,
+}
+
 struct Z80 {
     regs: [u8; 16],
 
@@ -30,6 +39,8 @@ struct Z80 {
     sp: u16,
     pc: u16,
 
+    flags: Z80Flags,
+
     mem: Memory,
 }
 
@@ -38,6 +49,7 @@ impl Z80 {
         Z80 {
             regs: [0; 16],
             i: 0, r: 0, ix: 0, iy: 0, sp: 0, pc:0,
+            flags: Z80Flags { s: false, z: false, h: false, p: false, n: false, c: false },
             mem: [0;65536]
         }
     }
